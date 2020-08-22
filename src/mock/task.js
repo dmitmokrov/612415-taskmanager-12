f29722dac0045d0934414c984b949bd29a8c7a99
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-08-22 16:32:39 +0300

    Добавляет генерацию id в моках, реализует "умную" очистку списка задач

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,6 +1,7 @@
 import {COLORS} from '../const.js';
 import {getRandomInteger} from '../utils/common.js';
 
+const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
 
 const generateDescription = () => {
   const descriptions = [
@@ -56,6 +57,7 @@ export const generateTask = () => {
   const color = getRandomColor();
 
   return {
+    id: generateId(),
     description,
     dueDate,
     repeating,
--- src/presenter/board.js
+++ src/presenter/board.js
@@ -15,6 +15,7 @@ export default class Board {
     this._boardContainer = boardContainer;
     this._renderedTaskCount = TASK_COUNT_PER_STEP;
     this._currentSortType = SortType.DEFAULT;
+    this._taskPresenter = {};
 
     this._boardComponent = new BoardView();
     this._sortComponent = new SortView();
@@ -71,6 +72,7 @@ export default class Board {
   _renderTask(task) {
     const taskPresenter = new TaskPresenter(this._taskListComponent);
     taskPresenter.init(task);
+    this._taskPresenter[task.id] = taskPresenter;
   }
 
   _renderTasks(from, to) {
@@ -99,7 +101,8 @@ export default class Board {
   }
 
   _clearTaskList() {
-    this._taskListComponent.getElement().innerHTML = ``;
+    // this._taskListComponent.getElement().innerHTML = ``;
+    Object.values(this._taskPresenter).forEach((presenter) => presenter.destroy());
     this._renderedTaskCount = TASK_COUNT_PER_STEP;
   }
 
