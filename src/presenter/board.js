ae353f390d3a143adccdabf0772cb8352e15df62
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-08-22 17:20:21 +0300

    Добавляет логику обновления задачи

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -4,6 +4,7 @@ import TaskListView from '../view/task-list.js';
 import NoTaskView from '../view/no-task.js';
 import LoadMoreButtonView from '../view/load-more-button.js';
 import TaskPresenter from './task.js';
+import {updateItem} from '../utils/common.js';
 import {render, RenderPosition, remove} from '../utils/render.js';
 import {sortTaskUp, sortTaskDown} from '../utils/task.js';
 import {SortType} from '../const.js';
@@ -25,6 +26,7 @@ export default class Board {
 
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
     this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
+    this._handleTaskChange = this._handleTaskChange.bind(this);
   }
 
   init(boardTasks) {
@@ -35,6 +37,12 @@ export default class Board {
     render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
 
     this._renderBoard();
+  }
+
+  _handleTaskChange(updatedTask) {
+    this._boardTasks = updateItem(this._boardTasks, updatedTask);
+    this._sourcedBoardTasks = updateItem(this._sourcedBoardTasks, updatedTask);
+    this._taskPresenter[updatedTask.id].init(updatedTask);
   }
 
   _sortTasks(sortType) {
@@ -70,7 +78,7 @@ export default class Board {
   }
 
   _renderTask(task) {
-    const taskPresenter = new TaskPresenter(this._taskListComponent);
+    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleTaskChange);
     taskPresenter.init(task);
     this._taskPresenter[task.id] = taskPresenter;
   }
--- src/presenter/task.js
+++ src/presenter/task.js
@@ -3,8 +3,9 @@ import TaskEditView from '../view/task-edit.js';
 import {render, RenderPosition, replace, remove} from '../utils/render.js';
 
 export default class Task {
-  constructor(taskListContainer) {
+  constructor(taskListContainer, changeData) {
     this._taskListContainer = taskListContainer;
+    this._changeData = changeData;
     this._task = null;
     this._taskComponent = null;
     this._taskEditComponent = null;
@@ -12,6 +13,8 @@ export default class Task {
     this._handleEditClick = this._handleEditClick.bind(this);
     this._handleFormSubmit = this._handleFormSubmit.bind(this);
     this._escKeyDownHandler = this._escKeyDownHandler.bind(this);
+    this._handleFavoriteClick = this._handleFavoriteClick.bind(this);
+    this._handleArchiveClick = this._handleArchiveClick.bind(this);
   }
 
   init(task) {
@@ -25,6 +28,8 @@ export default class Task {
 
     this._taskComponent.setEditClickHandler(this._handleEditClick);
     this._taskEditComponent.setFormSubmitHandler(this._handleFormSubmit);
+    this._taskComponent.setFavoriteClickHandler(this._handleFavoriteClick);
+    this._taskComponent.setArchiveClickHandler(this._handleArchiveClick);
 
     if (prevTaskComponent === null || prevTaskEditComponent === null) {
       render(this._taskListContainer, this._taskComponent, RenderPosition.BEFOREEND);
@@ -69,7 +74,16 @@ export default class Task {
     this._replaceCardToForm();
   }
 
-  _handleFormSubmit() {
+  _handleFormSubmit(task) {
+    this._changeData(task);
     this._replaceFormToCard();
+  }
+
+  _handleFavoriteClick() {
+    this._changeData(Object.assign({}, this._task, {isFavorite: !this._task.isFavorite}));
+  }
+
+  _handleArchiveClick() {
+    this._changeData(Object.assign({}, this._task, {isArchive: !this._task.isArchive}));
   }
 }
