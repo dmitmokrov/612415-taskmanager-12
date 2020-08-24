223c65be9786360551e58a19922467ba446b6813
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-08-24 20:29:56 +0300

    Добавляет smart компонент

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -1,4 +1,4 @@
-import AbstractView from './abstract.js';
+import SmartView from './smart.js';
 import {COLORS} from '../const';
 import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate} from '../utils/task.js';
 
@@ -131,7 +131,7 @@ const createCardEditElement = (data) => {
   </article>`;
 };
 
-export default class TaskEdit extends AbstractView {
+export default class TaskEdit extends SmartView {
   constructor(task = BLANK_TASK) {
     super();
     this._data = TaskEdit.parseTaskToData(task);
@@ -147,33 +147,6 @@ export default class TaskEdit extends AbstractView {
 
   getTemplate() {
     return createCardEditElement(this._data);
-  }
-
-  updateData(update, justDataUpdating) {
-    if (!update) {
-      return;
-    }
-
-    this._data = Object.assign({}, this._data, update);
-
-    if (justDataUpdating) {
-      return;
-    }
-
-    this.updateElement();
-  }
-
-  updateElement() {
-    let prevElement = this.getElement();
-    const parent = prevElement.parentElement;
-    this.removeElement();
-
-    const newElement = this.getElement();
-
-    parent.replaceChild(newElement, prevElement);
-    prevElement = null;
-
-    this.restoreHandlers();
   }
 
   restoreHandlers() {
